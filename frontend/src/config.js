d10f2673e20df23211346edc1573c5ccc23e7149
Hariom Vashishtha <hariom.vashishtha@MB-M-HARIOMV.local>
2024-05-31 17:03:22 +0530

    change url to netlify

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL="https://online-hospital-server.vercel.app/api/v1";
+export const BASE_URL="https://onlinehospitalserver.netlify.app/.netlify/functions/index/api/v1";
+
 export const token=localStorage.getItem("token");

9a324335a0acbb89c940ef9706cd5fcc26df63f2
Hariom Vashishtha <hariom.vashishtha@MB-M-HARIOMV.local>
2024-06-30 10:51:24 +0530

    adding tab in mobile view in profile

--- frontend/src/Dashboard/doctor-account/Tabs.jsx
+++ frontend/src/Dashboard/doctor-account/Tabs.jsx
@@ -6,21 +6,26 @@ import { authContext } from "../../context/AuthContext";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 const Tabs = ({ tab, settab }) => {
   const { dispatch } = useContext(authContext);
   const navigate = useNavigate();
   const handleLogout = () => {
     dispatch({ type: "LOGOUT" });
     navigate("/login");
-  }
+  };
+  const [showMenu, setShowMenu] = React.useState(false);
   return (
     <div>
-      <span className="lg:hidden ">
+      <span className="lg:hidden" onClick={() => setShowMenu(!showMenu)}>
         <BiMenu className="w-6 h-6 cursor-pointer" />
       </span>
-      <div className="hidden lg:flex flex-col p-[30px] bg-[rgb(249,242,242)]  shadow-panelShadow items-center h-max rounded-md ml-[30px] ">
-        <button onClick={() => settab("overview")}
+      <div
+        className={`flex flex-col p-[30px] bg-[rgb(249,242,242)] shadow-panelShadow items-center h-max rounded-md ml-[30px] ${
+          !showMenu && "hidden"
+        }`}
+      >
+        <button
+          onClick={() => settab("overview")}
           className={` ${
             tab === "overview"
               ? "bg-indigo-100 text-primaryColor"
@@ -29,7 +34,8 @@ const Tabs = ({ tab, settab }) => {
         >
           Overview
         </button>
-        <button onClick={() => settab("appointments")}
+        <button
+          onClick={() => settab("appointments")}
           className={` ${
             tab === "appointments"
               ? "bg-indigo-100 text-primaryColor"
@@ -38,8 +44,9 @@ const Tabs = ({ tab, settab }) => {
         >
           Appointments
         </button>
-        
-        <button onClick={() => settab("settings")}
+
+        <button
+          onClick={() => settab("settings")}
           className={` ${
             tab === "settings"
               ? "bg-indigo-100 text-primaryColor"
@@ -48,19 +55,17 @@ const Tabs = ({ tab, settab }) => {
         >
           Profile
         </button>
-        <div className="mt-[100px] md:mt-[20px] w-full">
-                    <button onClick={handleLogout}
-                      className="w-full bg-[#181A1E] text-[15px] leading-7 p-3 rounded-md text-white"
-                    
-                    >
-                      Logout{" "}
-                    </button>
-                    <button className="w-full bg-red-600 selection:text-[15px]  text-white  mt-4 leading-7 p-3 rounded-md">
-                      Delete Account{" "}
-                    </button>
-                  </div>
-        
-        
+        <div className="mt-[100px] md:mt-[20px] sm:mt-0 w-full">
+          <button
+            onClick={handleLogout}
+            className="w-full bg-[#181A1E] text-[15px] leading-7 p-3 rounded-md text-white"
+          >
+            Logout{" "}
+          </button>
+          <button className="w-full bg-red-600 selection:text-[15px]  text-white  mt-4 leading-7 p-3 rounded-md">
+            Delete Account{" "}
+          </button>
+        </div>
       </div>
     </div>
   );
